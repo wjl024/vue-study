669184d819d61a6995914ddb2dd54d47daa86170
wjl024 <1433686098@qq.com>
2019-03-26 17:23:14 +0800

    upload files html

--- vue-rutter-demo1/src/components/NewCourse.vue
+++ vue-rutter-demo1/src/components/NewCourse.vue
@@ -1,17 +1,14 @@
 <template>
 	<div class="container">
-		<div class="location">当前位置:<router-link to="/">班课列表</router-link>>创建班课</div>
-		<div class="content">
-			<div>
-		<input class="input-box" type="text" placeholder="请输入班课名称" v-model="course.courseName" />
-		<input class="input-box" type="text" placeholder="请输入班级" v-model="course.courseClass" />
-		<input class="input-box" type="text" placeholder="请输入图片地址" v-model="course.cover" />
-		<div class="btn-container">
-			<button @click="addcourse(course)" class="btn">确定</button>
-			<button class="cancel">取消</button>
+		<input type="text" placeholder="请输入班课名称" v-model="courseName" class="input-box" />
+		<input type="text" placeholder="请输入班级" v-model="courseClass" class="input-box" />
+		<p>班课封面</p>
+		<div class="preview" @click="handleClick()">
+			<img :src="imgurl" class="cover" v-if="!show" />
+			<img src="../assets/plus-grey.png" class="icon-plus" v-if="show" />
+			<input type="file" @change="getFile($event) " style="display: none;" id="coverFile" />
 		</div>
-		</div>
-		</div>
+		<button @click="addcourse(course)" class="btn">确定</button>
 	</div>
 </template>
 
@@ -20,112 +17,97 @@
 		name: 'NewCourse',
 		data() {
 			return {
-				loginUserId:1,
-				course: {
+				loginUserId: 1,
 				courseName: '',
 				courseClass: '',
-				cover: ''
-			}
+				file: '',
+				imgurl: '',
+				show: true,
+				course:''
 			};
 		},
 		methods: {
+			//点击图片预览区，即模拟点击文件选择组件
+			handleClick: function() {
+				document.getElementById('coverFile').click();
+			},
+			//图片预览
+			getFile: function() {
+				this.file = event.target.files[0];
+				var windowURL = window.URL || window.webkitURL;
+				alert( windowURL.createObjectURL(this.file));
+				this.imgurl = windowURL.createObjectURL(this.file);
+				this.show = false;
+			},
 			addcourse: function(course) {
-               var _this = this;
-			this.$http({
-				method: 'post',
-				url: 'http://localhost:8080/api/course',
-				data: {
-					userId: _this.loginUserId,
-					courseName: course.courseName,
-					courseClass: course.courseClass,
-					cover: course.cover,
-					finished: 0
-				}
-			}).then(function() {
-				alert('新增班课成功');
-				_this.$router.push('/');
-			});
+				var _this=this;
+				var windowURL = window.URL || window.webkitURL;
+				this.$http({
+					method: 'post',
+					url: 'http://localhost:8080/api/course',
+					data: {
+						userId: _this.loginUserId,
+						courseName: _this.courseName,
+						courseClass: _this.courseClass,
+						cover: windowURL.createObjectURL(this.file),
+						finished: 0
+					}
+				}).then(function() {
+				  alert('新增班课成功');
+					_this.$router.push('/');
+				});
+			}
 		}
-	}
 	};
 </script>
 
 <style scoped="scoped">
 	.container {
-		width: 70%;
-		margin: 0 auto;
 		display: flex;
 		flex-direction: column;
 		padding-top: 20px;
 		padding-left: 100px;
-		background-color:rgb(244,244,244);
-		height: 100%;
-	}
-    .content{
-		width: 70%;
-		margin: 0 auto;
-		display: flex;
-		flex-direction: column;
-		padding-top: 20px;
+		background-color: #fff;
 		margin-top: 20px;
-		background-color:#fff;
-		height: 20%;
-		align-items: center;
 	}
-	.location{
-		width: 70%;
-		margin: 0 auto;
-	}
-	a{
-		color: #000000;
-	}
-	a:hover{
-		text-decoration: underline;
-	}
+
 	.input-box {
-		width: 100%;
-		height: 30px;
-		margin-bottom: 30px;
+		width: 500px;
+		height: 40px;
+		margin-bottom: 40px;
 		font-size: 15px;
 
 	}
 
 	.btn {
-		color: rgb(0, 187, 211);
-		outline: none;
-		border: 1px solid rgb(0, 187, 211);
-		background-color: #fff;
-		width: 100px;
+		margin-top: 20px;
+		width: 120px;
 		height: 40px;
+		border: 2px solid rgb(0, 187, 221);
+		background-color: #fff;
 		border-radius: 8px;
-	}
-	.btn:hover{
-		border: 1px solid skyblue;
-		color: skyblue;
-		cursor: pointer;
+		outline: none;
+		color: rgb(0, 187, 221);
+		font-size: 16px;
 	}
 
-	.cancel {
-		color: slategrey;
-		outline: none;
-		border: 1px solid slategrey;
-		background-color: #fff;
-		width: 100px;
-		height: 40px;
-		border-radius: 8px;
-		margin-left: 25px;
-	}
-	.cancel:hover{
-		color: lightgrey;
-		border:1px solid lightgrey;
+	.preview {
+		width: 150px;
+		height: 150px;
+		border: 2px dashed #aaa;
 		cursor: pointer;
-	}
-	.btn-container{
-		width: 100%;
-		height: 60px;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		background-color:#fff;
+	}
+
+	.icon-plus {
+		width: 60px;
+		height: 60px;
+	}
+
+	.cover {
+		width: 100%;
+		height: 100%;
 	}
 </style>
--- vue-rutter-demo1/src/components/Task.vue
+++ vue-rutter-demo1/src/components/Task.vue
@@ -1,19 +1,95 @@
 <template>
 	<div class="container">
 		<h3>任务中心</h3>
+		<div class="preview">
+			<img :src="imgurl" />
+		</div>
+		<form>
+			<div class="upload">
+				选择文件
+				<input type="file" @change="getFile($event)" />
+			</div>
+			<br />
+			<button @click="submit($event)" class="sub-btn">提交</button>
+		</form>
 	</div>
 </template>
 
 <script>
-	export default{
-		name:'Task',
-		data(){
-			return{
-				
+	export default {
+		name: 'Task',
+		data() {
+			return {
+				imgurl: 'https://static-cdn-oss.mosoteach.cn/mssvc/icons/icon_cc_cover1@2x.png',
+				file: ''
 			};
+		},
+		methods: {
+			//图片预览
+			getFile: function(event) {
+				this.file = event.target.files[0];
+				var windowURL = window.URL || window.webkitURL;
+				alert(windowURL.createObjectURL(this.file));
+				this.imgurl = windowURL.createObjectURL(this.file);
+			},
+			submit: function(event) {
+				//阻止元素发生默认行为
+				event.preventDefault();
+				let formData = new FormData();
+				formData.append('file', this.file);
+				this.$http.post('http://localhost:8080/upload', formData)
+					.then(function(response) {
+						alert(response.data);
+						this.imgurl = response.data;
+					});
+			}
 		}
-	}
+	};
 </script>
 
 <style scoped="scoped">
+	.preview {
+		width: 300px;
+		height: 300px;
+	}
+
+	.preview img {
+		width: 100%;
+		height: 100%;
+	}
+
+	.sub-btn {
+		width: 120px;
+		height: 35px;
+		border-radius: 10px;
+		outline: none;
+		background-color: #333333;
+		color: #ffffff;
+	}
+
+	.upload {
+		margin-top: 10px;
+		display: inline-block;
+		width: 120px;
+		height: 35px;
+		border: 1px solid #99d3f5;
+		border-radius: 4px;
+		background-color: #d0eef;
+		color: #000;
+		text-align: center;
+		cursor: pointer;
+	}
+
+	.upload input {
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+	}
+
+	.upload:hover {
+		background-color: #aadffd;
+		border-color: #78c3fc;
+		color: #004974;
+		text-decoration: none;
+	}
 </style>
